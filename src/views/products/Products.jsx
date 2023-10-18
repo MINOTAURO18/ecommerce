0e738f73789990ef3e4e0891aaf04f0df600f8ce
alexander muñoz <alexander12345my@gmail.com>
2023-10-18 18:43:00 -0500

    products 80%

--- src/views/products/Products.jsx
+++ src/views/products/Products.jsx
@@ -2,12 +2,15 @@ import styles from './products.module.css'
 import {products} from '../../data/products.json'
 const Products = () => {
     return(
-        <section className={`${styles.products}`}>
+        <section className={`${styles.products} py-28 px-8% flex flex-wrap justify-center gap-9`}>
             {
                 products.map(pro => {
                     return(
-                        <div key={pro.id}>
-                            <img className='w-20' src={pro.images[0]} alt="" />
+                        <div key={pro.id} className={`${styles.card} w-80 h-72`}>
+                            <img className='w-full h-52' src={pro.images[0]} alt="" />
+                            <h3>{pro.title}</h3>
+                            <span>{pro.price}</span>
+                            <button>Buy</button>
                         </div>
                     )
                 })
